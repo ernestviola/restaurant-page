2c1859f885bff98c2355c43ff5a35714a1b0dcea
Ernest V <ernest.viol@gmail.com>
2026-01-08 20:15:17 -0800

    Move html elements to module
    Created a new module called home.js each module will hold a different
    page and will be loaded into index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import './styles.css';
+import { hero } from './pages/home.js'
 
-console.log('Hello Restaurant');
+const content = document.querySelector('#content');
+const child = document.createElement('div')
+content.appendChild(child);
+content.appendChild(hero);
---
+++ src/pages/home.js
@@ -0,0 +1,41 @@
+/*
+<div class='hero'>
+  <img src='images/restaurant.jpg' alt='Picture of the Restaurant'>
+  <div class='hero__description'>
+    <h1>Dine with us</h1>
+    <p>Description of the restaurant and ambiance</p>
+  </div>
+</div>
+<div class='reservation__form'>
+  <form action=''>
+  </form>
+</div>
+
+*/
+
+import restaurantImg from '../images/restaurant.jpg';
+
+const hero = document.createElement('div');
+hero.className = 'hero';
+
+const img = document.createElement('img');
+img.src = restaurantImg;
+img.alt = 'Picture of the restaurant.';
+
+const heroDescription = document.createElement('div');
+heroDescription.className = 'hero__description';
+
+const heroDescriptionHeader = document.createElement('h1');
+heroDescriptionHeader.innerText = 'Dine with us!';
+
+const heroDescriptionParagraph = document.createElement('p');
+heroDescriptionParagraph.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt tempora eligendi labore iusto, reprehenderit perferendis natus? Accusantium aperiam atque magnam nobis rem voluptatibus, harum corrupti velit, quo earum, nostrum inventore voluptas ut. Est, eveniet natus. Fuga iste explicabo cumque voluptatem aliquid saepe animi perspiciatis et doloribus ut maiores, totam laborum facere hic possimus consequatur eum voluptate laboriosam nulla quidem cum recusandae labore? Molestiae adipisci eos repellendus rerum atque dolor deserunt saepe facere fuga sunt ducimus reiciendis voluptatem fugit aliquid exercitationem eligendi nesciunt optio, omnis perspiciatis corrupti dolores officia. Id, tempora doloribus dolore inventore eligendi corrupti debitis ab illum necessitatibus sit possimus beatae aliquid nostrum ratione placeat quod vero facere nam, corporis quaerat error perspiciatis cum velit aliquam. Aperiam, saepe modi?';
+
+heroDescription.appendChild(heroDescriptionHeader);
+heroDescription.appendChild(heroDescriptionParagraph);
+
+hero.appendChild(img);
+hero.appendChild(heroDescription);
+
+
+export { hero };
