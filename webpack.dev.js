c0a916033a2e9f43dbcdcfeb7f84dc91d2ce0aa5
Ernest V <ernest.viol@gmail.com>
2026-01-11 13:25:21 -0800

    Update webpack
    Added two new config files for prod and dev

---
+++ webpack.dev.js
@@ -0,0 +1,10 @@
+const { default: merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+})
---
+++ webpack.prod.js
@@ -0,0 +1,6 @@
+const { default: merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+});
